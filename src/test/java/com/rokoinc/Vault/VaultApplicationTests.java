0ab2c835d6ec10112e7811b2d5c0da4af15ccbf2
snr-roko <rbbagyei@outlook.com>
2025-07-13 15:23:07 +0000

    Initial commit

---
+++ src/test/java/com/rokoinc/Vault/VaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rokoinc.Vault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
