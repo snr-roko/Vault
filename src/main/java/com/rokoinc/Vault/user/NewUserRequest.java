74ef88248419e6f240cc422cd1a50db4a4d99444
snr-roko <rbbagyei@outlook.com>
2025-07-14 15:16:11 +0000

    Allow server error messages and error binding messages to be shown and add custom messages to validation.

--- src/main/java/com/rokoinc/Vault/user/NewUserRequest.java
+++ src/main/java/com/rokoinc/Vault/user/NewUserRequest.java
@@ -7,15 +7,15 @@ import jakarta.validation.constraints.Past;
 import java.time.LocalDate;
 
 public record NewUserRequest(
-        @NotNull String firstName,
-        @NotNull String lastName,
-        @Email String email,
-        @NotNull String phone,
-        @Past LocalDate dateOfBirth,
-        @NotNull String GPS,
-        @NotNull String city,
-        @NotNull String region,
-        @NotNull String zipCode,
-        @NotNull Gender gender
+        @NotNull(message = "First Name is required") String firstName,
+        @NotNull(message = "Last Name is required") String lastName,
+        @NotNull(message = "Email is required") @Email(message = "Email must be a valid email") String email,
+        @NotNull(message = "Phone number is required") String phone,
+        @NotNull(message = "Date of Birth is required") @Past(message = "Date of Birth must be in the past") LocalDate dateOfBirth,
+        @NotNull(message = "GPS is required") String GPS,
+        @NotNull(message = "City is required") String city,
+        @NotNull(message = "Region is required") String region,
+        @NotNull(message = "ZipCode is required") String zipCode,
+        @NotNull(message = "Gender is required") Gender gender
 ) {
 }
