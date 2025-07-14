f9d003782867d4e92d03aa51111694f9271fd236
snr-roko <rbbagyei@outlook.com>
2025-07-14 06:55:32 +0000

    Update User Model to use class instead of record.

--- src/main/java/com/rokoinc/Vault/VaultApplication.java
+++ src/main/java/com/rokoinc/Vault/VaultApplication.java
@@ -26,13 +26,151 @@ public class VaultApplication {
 	public enum Gender{MALE, FEMALE}
 
 
-	// simple user model with record
-	public record User(
-			Integer id, String firstName, String lastName, String email, String phone,
-			LocalDate dateOfBirth, String GPS, String city, String region, String zipCode, Gender gender,
-			LocalDate createdAt, LocalDate updatedAt
-	) {
+//	// simple user model with record
+//	public record User(
+//			Integer id, String firstName, String lastName, String email, String phone,
+//			LocalDate dateOfBirth, String GPS, String city, String region, String zipCode, Gender gender,
+//			LocalDate createdAt, LocalDate updatedAt
+//	) {
+//
+//	}
 
+	// User Model Class
+	public static class User{
+		private Integer id;
+		private String firstName;
+		private String lastName;
+		private String email;
+		private String phone;
+		private LocalDate dateOfBirth;
+		private String GPS;
+		private String city;
+		private String region;
+		private String zipCode;
+		private Gender gender;
+		private LocalDate createdAt;
+		private LocalDate updatedAt;
+
+
+		public User(Integer id, String firstName, String lastName, String email, String phone, LocalDate dateOfBirth, String GPS, String city, String region, String zipCode, Gender gender, LocalDate createdAt, LocalDate updatedAt) {
+			this.id = id;
+			this.firstName = firstName;
+			this.lastName = lastName;
+			this.email = email;
+			this.phone = phone;
+			this.dateOfBirth = dateOfBirth;
+			this.GPS = GPS;
+			this.city = city;
+			this.region = region;
+			this.zipCode = zipCode;
+			this.gender = gender;
+			this.createdAt = createdAt;
+			this.updatedAt = updatedAt;
+		}
+
+		public Integer getId() {
+			return id;
+		}
+
+		public void setId(Integer id) {
+			this.id = id;
+		}
+
+		public String getFirstName() {
+			return firstName;
+		}
+
+		public void setFirstName(String firstName) {
+			this.firstName = firstName;
+		}
+
+		public String getLastName() {
+			return lastName;
+		}
+
+		public void setLastName(String lastName) {
+			this.lastName = lastName;
+		}
+
+		public String getEmail() {
+			return email;
+		}
+
+		public void setEmail(String email) {
+			this.email = email;
+		}
+
+		public String getPhone() {
+			return phone;
+		}
+
+		public void setPhone(String phone) {
+			this.phone = phone;
+		}
+
+		public LocalDate getDateOfBirth() {
+			return dateOfBirth;
+		}
+
+		public void setDateOfBirth(LocalDate dateOfBirth) {
+			this.dateOfBirth = dateOfBirth;
+		}
+
+		public String getGPS() {
+			return GPS;
+		}
+
+		public void setGPS(String GPS) {
+			this.GPS = GPS;
+		}
+
+		public String getCity() {
+			return city;
+		}
+
+		public void setCity(String city) {
+			this.city = city;
+		}
+
+		public String getRegion() {
+			return region;
+		}
+
+		public void setRegion(String region) {
+			this.region = region;
+		}
+
+		public String getZipCode() {
+			return zipCode;
+		}
+
+		public void setZipCode(String zipCode) {
+			this.zipCode = zipCode;
+		}
+
+		public Gender getGender() {
+			return gender;
+		}
+
+		public void setGender(Gender gender) {
+			this.gender = gender;
+		}
+
+		public LocalDate getCreatedAt() {
+			return createdAt;
+		}
+
+		public void setCreatedAt(LocalDate createdAt) {
+			this.createdAt = createdAt;
+		}
+
+		public LocalDate getUpdatedAt() {
+			return updatedAt;
+		}
+
+		public void setUpdatedAt(LocalDate updatedAt) {
+			this.updatedAt = updatedAt;
+		}
 	}
 
 	// automatically incrementing id
@@ -130,18 +268,18 @@ public class VaultApplication {
 	@GetMapping
 	public ResponseEntity<List<User>> getAllUsers(@RequestParam(value = "sort", defaultValue = "ASC") SortingOrder sort) {
 		if (sort == SortingOrder.ASC) {
-			List<User> response = users.stream().sorted(Comparator.comparing(User::id)).collect(Collectors.toList());
+			List<User> response = users.stream().sorted(Comparator.comparing(User::getId)).collect(Collectors.toList());
 
 			return ResponseEntity.ok().body(response);
 		}
 
-		List<User> response = users.stream().sorted(Comparator.comparing(User::id).reversed()).collect(Collectors.toList());
+		List<User> response = users.stream().sorted(Comparator.comparing(User::getId).reversed()).collect(Collectors.toList());
 		return ResponseEntity.ok().body(response);
 	}
 
 	@GetMapping("{id}")
 	public ResponseEntity<Optional<User>> getUserById (@PathVariable("id") Integer id) {
-        Optional<User> response = users.stream().filter(user -> user.id.equals(id)).findFirst();
+        Optional<User> response = users.stream().filter(user -> user.id.equals(id)).findFirst ();
 
 		return ResponseEntity.ok(response);
 	}
