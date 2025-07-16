078444aa189227f949bced0aea19c6b50ee42dd0
snr-roko <rbbagyei@outlook.com>
2025-07-16 16:22:09 +0000

    Add Scheduling task to print app name on console every 30 seconds

--- src/main/java/com/rokoinc/Vault/VaultApplication.java
+++ src/main/java/com/rokoinc/Vault/VaultApplication.java
@@ -2,12 +2,24 @@ package com.rokoinc.Vault;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
+@EnableScheduling
 public class VaultApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VaultApplication.class, args);
+	}
+
+	@Scheduled(fixedRate = 30, timeUnit = TimeUnit.SECONDS)
+	public void displayApplicationName() throws InterruptedException {
+		System.out.println("*************************Vault*************************");
+		Thread.sleep(5000);
+		System.out.println("New Features Coming");
 	}
 
 }
